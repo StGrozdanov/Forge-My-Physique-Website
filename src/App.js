7e3310e55cdd7b493c764ab34d67514973d39811
Stoyan Grozdanov <st.grozdanov@abv.bg>
2022-05-06 18:25:55 +0300

    Implemented features section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navigation from './components/Navigation/Navigation';
 import Header from './components/Header/Header';
+import Features from './components/Features/Features';
 
 function App() {
   return (
     <>
       <Navigation />
       <Header />
+      <Features />
     </>
   );
 }
--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -1,16 +1,19 @@
+import './Features.css';
+
 export default function Features() {
     return (
-        <>
-            <p>
+        <article className="features-article">
+            <h2>Forge my physique</h2>
+            <p className='slideFadeInUp' style={{animationDelay: '0.5s'}}>
                 Your training exercises, your nutrition, your measurements, your progress pictures, all in one
                 application.
             </p>
-            <p>
+            <p className='slideFadeInUp' style={{animationDelay: '1s'}}>
                 Get tips for your training, compare your training sessions against your previous performances.
                 It's you against you. Feeling competitive? We won't stop you from comparing your sessions against
                 your pears as well.
             </p>
-            <p>
+            <p className='slideFadeInUp' style={{animationDelay: '1.5s'}}> 
                 You want to cut, maintain or gain weight? We will handle that for you. Calculating your calories is
                 easy work. Providing your gender and base physique stats is all we need. You will gain access to
                 weight management charts, recording your weight will help us to adjust your calorie and macro intakes
@@ -18,13 +21,13 @@ export default function Features() {
                 percentage and weekly intake nutrition tables, that will give you the freedom to be flexible in your
                 food picking choices.
             </p>
-            <p>
+            <p className='slideFadeInUp' style={{animationDelay: '2s'}}>
                 Training for some time but not sure if you really have progress on your physique? Our training
                 effectiveness algoritms will have you covered. You can track your muscle load progress after every
                 strength training session, we also track your measurement progress up to the last circumference mm.
                 Our ranking system will keep you on track - providing you with PERSONAL physique measurement goals at
                 any stage of your training journey, depending on your height and gender.
             </p>
-        </>
+        </article>
     );
 }
