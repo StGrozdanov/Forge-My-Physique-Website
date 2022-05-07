f499837279acf96333522bfdba4f4cc8db23c35b
Stoyan Grozdanov <st.grozdanov@abv.bg>
2022-05-07 22:34:57 +0300

    Implemented back to top responsive design

--- src/components/BackToTop/BackToTop.js
+++ src/components/BackToTop/BackToTop.js
@@ -3,7 +3,7 @@ import './BackToTop.css';
 export default function BackToTop() {
     return (
         <a href="#" id="scrollToTop" className="button-to-top" style={{ display: "block" }}>
-            <i class="fa-solid fa-angle-up"></i>
+            <i className="fa-solid fa-angle-up"></i>
         </a>
     );
 }
